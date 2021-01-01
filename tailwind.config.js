d018b2399d330a1f31876768b9e2b6bd59eee087
Manav Misra <manavm@visionwebsoft.com>
2021-01-01 16:14:59 -0600

    :wrench: Extend tailwind mono font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        mono: ["Ubuntu", ...defaultTheme.fontFamily.mono],
+      },
       width: {
         "50vw": "50vw",
       },
