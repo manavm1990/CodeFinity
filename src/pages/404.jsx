e19f4c99c2456255ccfd0aa80ac05a2baca8d737
Manav Misra <manavm@visionwebsoft.com>
2020-12-31 18:11:59 -0600

    Customize 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,12 +1,12 @@
+import { Layout, SEO } from "components"
+import { Link } from "gatsby"
 import React from "react"
-import Layout from "../components/Layout"
-import SEO from "../components/SEO"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404: Not found | CodeFinity" />
+    <h1>404: Not Found 🙅🏾‍♂️</h1>
+    <Link to="/">Back Home 🏠?</Link>
   </Layout>
 )
 
