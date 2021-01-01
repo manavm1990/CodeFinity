679ce72dd0e28b9c42aa6978e65e7ac6fb3e3f14
Manav Misra <manavm@visionwebsoft.com>
2020-12-31 18:14:56 -0600

    :construction: Render starter home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,11 @@
-import { Link } from "gatsby"
+import { SEO } from "components"
 import React from "react"
-import Image from "../components/Image"
-import Layout from "../components/Layout"
-import SEO from "../components/SEO"
 
 const IndexPage = () => (
-  <Layout>
+  <>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div className="max-w-xs mb-4">
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to &quot;Using TypeScript&quot;</Link>
-  </Layout>
+    <h1>CodeFinity</h1>
+  </>
 )
 
 export default IndexPage
