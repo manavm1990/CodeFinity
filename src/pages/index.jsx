ee142be1f9f00dedddedc5b10d92572cd52ca6c0
Manav Misra <manavm@visionwebsoft.com>
2021-01-01 20:27:57 -0600

    Framer animate button
    Removing 'gray hover' border.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import { Button, Logo, SEO, Video } from "components"
 import "fontsource-ubuntu" // Defaults to weight 400 with all styles included.
+import { motion } from "framer-motion"
 import React from "react"
 
 const IndexPage = () => (
@@ -8,10 +9,16 @@ const IndexPage = () => (
     <Video />
     <div className="h-screen flex flex-col justify-center items-center">
       <Logo styles="max-w-xs w-50vw" />
-      <Button
-        text=" 👨🏾‍🎓 Students"
-        styles="bg-btn hover:bg-gray-50 border-2 border-white hover:border-gray-500 rounded hover:rounded-3xl hover:shadow font-mono mt-8 text-yellow-500 hover:text-green-400 tracking-widest transform hover:scale-110 transition-all uppercase p-3"
-      />
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1.1 }}
+      >
+        <Button
+          text=" 👨🏾‍🎓 Students"
+          styles="bg-btn hover:bg-gray-50 border-2 border-white rounded hover:rounded-3xl hover:shadow font-mono mt-8 text-yellow-500 hover:text-green-400 tracking-widest transform hover:scale-110 transition-all uppercase p-3"
+        />
+      </motion.div>
     </div>
   </>
 )
