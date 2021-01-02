920a8bf604942efb3310e213146fc52eec61a3c2
Manav Misra <manavm@visionwebsoft.com>
2021-01-02 09:55:51 -0600

    Add vampire conditional

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import { Button, Logo, SEO, Video } from "components"
 import "fontsource-ubuntu" // Defaults to weight 400 with all styles included.
 import { motion } from "framer-motion"
+import { getVampire } from "lib"
 import React from "react"
 
 const IndexPage = () => (
@@ -15,7 +16,7 @@ const IndexPage = () => (
         transition={{ delay: 1.1 }}
       >
         <Button
-          text=" 👨🏾‍🎓 Curricula"
+          text={`👨🏾‍🎓 Curricula${getVampire()}`}
           styles="bg-btn hover:bg-gray-50 border-2 border-white rounded hover:rounded-3xl hover:shadow font-mono mt-8 text-yellow-500 hover:text-green-400 tracking-widest transform hover:scale-110 transition-all uppercase p-3"
         />
       </motion.div>
