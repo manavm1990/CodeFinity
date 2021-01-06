bdab2cf076ea0db9fcde704e740c48b7618f8713
Manav Misra <manavm@visionwebsoft.com>
2021-01-06 10:01:37 -0600

    Integrate eslint with prettier react reco, etc.

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  extends: [
+    "react-app",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "prettier",
+    "prettier/react",
+  ],
+}
